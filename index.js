8898ce5c56995ace167c9a083513f027e12f3347
Pedro <PEDROESAUCOSAJAYVELASQUEZ@gmail.com>
2025-03-11 23:04:45 -0600

    Conexion de base de datos y peticion get con datos

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+const { constants } = require('buffer');
 const express = require('express');
+const { Client } = require('pg');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 app.use(express.json());
 
-app.get('/User', (req, res) => {
-    res.status(200);
-    res.send("Pruebas para consumo de la api")
-    console.log("gay angel")
+const query = 'SELECT * FROM ruta'
+
+
+
+const client  = new Client( {
+    user: 'postgres',       // Usuario de PostgreSQL
+    host: 'localhost',      // Servidor donde está la base de datos (en este caso, local)
+    database: 'Ruta',       // Nombre de la base de datos a la que se quiere conectar
+    password: '1234',       // Contraseña del usuario de PostgreSQL
+    port: 3000,             // Puerto en el que se intenta conectar (esto es un posible error)
 });
+
+
+client.connect()
+.then (()=>console.log("la conexion de datos fue exitosa"))
+.catch(Error=>console.log("Error en la conexion ",Error))
+
+
+
 
 app.listen(PORT, (error) =>
 {
@@ -17,3 +33,19 @@ app.listen(PORT, (error) =>
         console.log("server error")
     }
 });
+
+client.query(query, (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    const rows = res.rows
+    
+        app.get('/User', (req, res) => {
+            res.status(200).json(rows);
+            console.log("Datos mostrados con exito")
+        });
+
+    client.end();
+});
+
